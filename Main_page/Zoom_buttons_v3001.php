87078f9fb77d3a90d42338f3de72b136daa03edf
TomHirsch3000 <35623024+TomHirsch3000@users.noreply.github.com>
2018-01-20 09:23:24 +0100

    First upload

---
+++ Main_page/Zoom_buttons_v3001.php
@@ -0,0 +1,16 @@
+ 		<button class="zarea" id="zarea" style="left:0px;top:0px;" 			onclick ="replace_content('1')"> 		+		</button>	
+		<button class="zarea" id="zarea2" style="left:250px;top:0px;"		onclick ="replace_content('2')"> 		+		</button>	
+		<button class="zarea" id="zarea3" style="left:500px;top:0px;"		onclick ="replace_content('3')"> 		+		</button>	
+		<button class="zarea" id="zarea4" style="left:0px;top:250px;"		onclick ="replace_content('4')"> 		+		</button>	
+		<button class="zarea" id="zarea5" style="left:250px;top:250px;"		onclick ="replace_content('5')"> 		+		</button>		
+		<button class="zarea" id="zarea6" style="left:500px;top:250px;"		onclick ="replace_content('6')"> 		+		</button>		
+		<button class="zarea" id="zarea7" style="left:0px;top:500px;"		onclick ="replace_content('7')"> 		+		</button>	
+		<button class="zarea" id="zarea8" style="left:250px;top:500px;"		onclick ="replace_content('8')"> 		+		</button>		
+		<button class="zarea" id="zarea9" style="left:500px;top:500px;"		onclick ="replace_content('9')"> 		+		</button>
+	
+		<button class="zarea" id="zarea10" style="left:750px;top:0px;"		onclick ="replace_content('3')"> 		+		</button>	
+		<button class="zarea" id="zarea11" style="left:750px;top:250px;"	onclick ="replace_content('6')"> 		+		</button>		
+		<button class="zarea" id="zarea12" style="left:750px;top:500px;"	onclick ="replace_content('9')"> 		+		</button>
+	
+
+	
