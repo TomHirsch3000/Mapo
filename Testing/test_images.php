54d2155f2610240e394a9ffcc2926477123e8801
TomHirsch3000 <35623024+TomHirsch3000@users.noreply.github.com>
2018-01-20 09:30:10 +0100

    Create test_images.php

---
+++ Testing/test_images.php
@@ -0,0 +1,51 @@
+
+
+<p style="text-align:center;z-index:-1"><img id="myimage" 
+		src="images/relativity_light_bending.jpg"
+		
+		width=200px
+		alt="images/relativity_light_bending.jpg" 
+		longdesc="images/relativity_light_bending.jpg" />  
+		</p> 	
+
+
+<p style="text-align:center;z-index:-1"><img id="myimage" 
+		src="images/Eratosthenes-Circumference.jpg"
+		
+		width=100px
+		alt="images/Eratosthenes-Circumference.jpg" 
+		longdesc="images/Eratosthenes-Circumference.jpg.jpg" />  
+		</p> 	
+		
+		<p style="text-align:center;z-index:-1"><img id="myimage" 
+		src="images/Kite-key-and-lightning.jpg"
+		
+		width=100px
+		alt="images/Kite-key-and-lightning.jpg" 
+		longdesc="images/Kite-key-and-lightning.jpg" />  
+		</p> 	
+
+		<p style="text-align:center;z-index:-1"><img id="myimage" 
+		src="images/Kite-key-and-lightning.jpg"
+		
+		width=50px
+		alt="images/Kite-key-and-lightning.jpg" 
+		longdesc="images/Kite-key-and-lightning.jpg" />  
+		</p> 	
+
+				<p style="text-align:center;z-index:-1"><img id="myimage" 
+		src="images/Kite-key-and-lightning.jpg"
+		
+		width=20px
+		alt="images/Kite-key-and-lightning.jpg" 
+		longdesc="images/Kite-key-and-lightning.jpg" />  
+		</p> 	
+
+		
+				<p style="text-align:center;z-index:-1"><img id="myimage" 
+		src="images/Kite-key-and-24.jpg"
+		
+		width=200px
+		alt="images/Kite-key-and-24.jpg" 
+		longdesc="images/Kite-key-and-24.jpg" />  
+		</p> 	
