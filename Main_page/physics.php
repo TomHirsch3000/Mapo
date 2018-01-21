e81804df09aa268442b8693be7e4f8e0967990ef
TomHirsch3000 <35623024+TomHirsch3000@users.noreply.github.com>
2018-01-21 09:22:20 +0100

    clean css

--- Main_page/physics.php
+++ Main_page/physics.php
@@ -27,27 +27,25 @@ body {
 }
 
 /* ~~ Element/tag selectors ~~ */
-ul, ol, dl { /* Due to variations between browsers, it's best practices to zero padding and margin on lists.
-&nbsp;For consistency, you can either specify the amounts you want here, or on the list items (LI, DT, DD) they contain.
-&nbsp;Remember that what you do here will cascade to the .nav list unless you write a more specific selector. */
+ul, ol, dl { 
 	padding: 0;
 	margin: 0;
 }
 
 /*applies to all elements within a DIV*/
 h1, h2, h3, h4, h5, h6, p {
-	margin-top: 0;	 /* removing the top margin gets around an issue where margins can escape from their containing div. The remaining bottom margin will hold it away from any elements that follow. */
+	margin-top: 0;	 
 	padding-right: 0px;
-	padding-left: 0px; /* adding the padding to the sides of the elements within the divs, instead of the divs themselves, gets rid of any box model math. A nested div with side padding can also be used as an alternate method. */
+	padding-left: 0px; /
 }
-a img { /* this selector removes the default blue border displayed in some browsers around an image when it is surrounded by a link */
+a img { 
 	border: none;
 }
 
-/* ~~ Styling for your site's links must remain in this order - including the group of selectors that create the hover effect. ~~ */
+
 a:link {
 	color: black;
-	text-decoration: underline; /* unless you style your links to look extremely unique, it's best to provide underlines for quick visual identification */
+	text-decoration: underline; 
 }
 a:visited {
 	color: #739611;
@@ -59,29 +57,26 @@ a:hover, a:active, a:focus { /* this group of selectors will give a keyboard nav
 
 /* ~~ This grouped selector gives the lists in the .content area space ~~ */
 .content ul, .content ol { 
-	padding: 0 15px 15px 0px; /* this padding mirrors the right padding in the headings and paragraph rule above. Padding was placed on the bottom for space between other elements on the lists and on the left to create the indention. These may be adjusted as you wish. */
+	padding: 0 15px 15px 0px; 
 }
 
-/* ~~ The navigation list styles (can be removed if you choose to use a premade flyout menu like Spry) ~~ */
 ul.nav {
-	list-style: none; /* this removes the list marker */
-	/*border-top: 1px solid #666;*/ /* this creates the top border for the links - all others are placed using a bottom border on the LI */
-	margin-bottom: 15px; /* this creates the space between the navigation on the content below */
+	list-style: none; 
+	margin-bottom: 15px; 
 }
 ul.nav li {
 text-align:center;
-/*border-top: 1px solid #666;
-	border-bottom: 1px solid #666;*/ /* this creates the button separation */
+
 }
-ul.nav a, ul.nav a:visited { /* grouping these selectors makes sure that your links retain their button look even after being visited */
+ul.nav a, ul.nav a:visited { 
 	padding: 5px 5px 5px 0px;
-	display: block; /* this gives the link block properties causing it to fill the whole LI containing it. This causes the entire area to react to a mouse click. */
-	width: 195px;  /*this width makes the entire button clickable for IE6. If you don't need to support IE6, it can be removed. Calculate the proper width by subtracting the padding on this link from the width of your sidebar container. */
+	display: block; 
+	width: 195px;  
 	text-decoration: none;
 	background: #87D86D
 ;
 }
-ul.nav a:hover, ul.nav a:active, ul.nav a:focus { /* this changes the background and text color for both mouse and keyboard navigators */
+ul.nav a:hover, ul.nav a:active, ul.nav a:focus { 
 	background: #527000;
 	color: #739611;
 }
@@ -141,20 +136,19 @@ margin:0px;
 	background: #A1D290;
 	position: relative;
 	color:#186400;
-	clear: both; /* this clear property forces the .container to understand where the columns end and contain them */
+	clear: both; 
 }
 
 /* ~~ miscellaneous float/clear classes ~~ */
-.fltrt {  /* this class can be used to float an element right in your page. The floated element must precede the element it should be next to on the page. */
+.fltrt {  
 	float: right;
 	margin-left: 8px;
 }
-.fltlft { /* this class can be used to float an element left in your page. The floated element must precede the element it should be next to on the page. */
+.fltlft { 
 	float: left;
 	margin-right: 8px;
 }
-.clearfloat { /* this class can be placed on a <br /> or empty div as 
-the final element following the last floated div (within the #container) if the #footer is removed or taken out of the #container */
+.clearfloat { 
 	clear:both;
 	height:0;
 	font-size: 1px;
@@ -164,7 +158,7 @@ the final element following the last floated div (within the #container) if the 
 .container {
 	width: 1210px;
 	background:white;
-	margin: 0 auto; /* the auto value on the sides, coupled with the width, centers the layout */
+	margin: 0 auto; 
 }
 
 .header {
@@ -204,7 +198,7 @@ the final element following the last floated div (within the #container) if the 
 	width: 1000px;
 	height:750px;
 	position:relative;
-	left:3px;
+	left:10px;
 	top:53px;
 	overflow-y: hidden;
 	overflow-x: hidden;
@@ -214,8 +208,6 @@ the final element following the last floated div (within the #container) if the 
 .node1
 {
 background:white;
-//border-style:solid;
-//rgba(135, 216, 109,0.6);
 color:black;
 border-radius: 10px;
 border-style:solid;
